6e1b094279c854fcb0a6d9f054ace518164d0e0a
Brett R. Gebbie <brg078@gmail.com>
2023-01-03 10:43:05 -0600

    initial work on delete question functionality

--- src/components/ListItemQuestion/ListItemQuestion.jsx
+++ src/components/ListItemQuestion/ListItemQuestion.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function ListItemQuestion(props) {
+
     const question = props.question;
+    const dispatch = useDispatch();
+
+
+    function approveQuestion(){
+        console.log('in approveQuestion!');
+    }
+        
+    function deleteQuestion(){
+        console.log('in deleteQuestion!',question.id);
+        alert('Are you sure you want to delete ADD ALERT LATER?');
+        dispatch({
+            type: 'DELETE_QUESTION', payload: question.id
+        });
+    }
 
 
     return (
         <>
             <td>{question.username}</td>
             <td>{question.question}</td>
-            <td>{JSON.stringify(question.flagged)}</td>
+            <td>{(question.approved? 'Yes':'No')}</td>
             <td>{(question.flagged? 'Yes':'No')}</td>
+            <td><button onClick={approveQuestion}>Approve</button></td>
+            <td><button>Mark As Flagged</button></td>
+            <td><button onClick={deleteQuestion}>Delete</button></td>
         </>
     )
 }
--- src/components/ListOfQuestions/ListOfQuestions.jsx
+++ src/components/ListOfQuestions/ListOfQuestions.jsx
@@ -3,11 +3,11 @@ import React, {useEffect} from 'react';
 //import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 //import rootReducer from '../../redux/reducers/_root.reducer';
-//import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import ListItemQuestion from '../ListItemQuestion/ListItemQuestion';
 
 function ListOfQuestions(){
-    //const history = useHistory();
+    const history = useHistory();
     const dispatch = useDispatch();
     const questions = useSelector(store => store.questionList);
 
@@ -20,25 +20,36 @@ function ListOfQuestions(){
         console.log('dom side question store', questions);
     }
 
+    const pushQuestions = () => {
+        console.log('in pushQuestions!');
+    }
+
+    const returnToAskQuestions = () => {
+        console.log('in returnToAskQuestions!');
+        history.push('/questions');
+    }
+
     return (
         <div>
             <h2>List</h2>
-            <p>{JSON.stringify(questions)}</p>
+            {/* <p>{JSON.stringify(questions)}</p> */}
             <table>
                 <th>User</th>
                 <th>Question</th>
+                <th>Approved?</th>
                 <th>Flagged?</th>
-                <th>Flagged?</th>
+                <th>Approve</th>
+                <th>Mark As Flagged</th>
+                <th>Delete</th>
                     {questions.map(question => (
                         <tr>
                             <ListItemQuestion key={question.id} question ={question}/>
                         </tr>
                     ))}
-                
-
-
             </table>
             <button onClick={getQuestions}>Get Questions List</button>
+            <button onClick={pushQuestions}>Push Approved Questions</button>
+            <button onClick={returnToAskQuestions}>Return to Ask Questions</button>
             
         </div>
     )
