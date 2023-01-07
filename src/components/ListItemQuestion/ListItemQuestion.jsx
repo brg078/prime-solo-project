fe7140456758d4a6f67ce994f20bacc3db1eafc7
Brett R. Gebbie <brg078@gmail.com>
2023-01-07 17:37:32 -0600

    material ui functionality on questions page mostly done

--- src/components/ListItemQuestion/ListItemQuestion.jsx
+++ src/components/ListItemQuestion/ListItemQuestion.jsx
@@ -1,5 +1,84 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Card, CardActionArea, CardHeader, CardContent } from '@mui/material';
+import { amber,deepPurple,cyan,lightGreen, grey } from '@mui/material/colors';
+import { ThemeProvider, withStyles} from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
+
+
+
+
+const QuestionCard = withStyles({
+    
+    root: {
+        //color: (question.flagged? amber[500]:cyan[500]),
+        raised: true,
+        //minWidth: '1200px',
+        width:'1250px',
+        //maxWidth: '1500px',
+        letterSpacing: '0.2rem',
+        textDecoration: 'none',
+        padding: '1px',
+        margin: '0.5rem',
+        //backgroundColor: deepPurple[500],
+        '&:hover': {transform: "scale3d(1.005, 1.0005, -33)"},
+
+    }
+})(Card);
+
+
+const FormControli = withStyles({
+    
+    root: {
+        padding: '1px',
+        margin: '0.5rem',
+    }
+})(FormControl);
+
+
+const Cardfront = withStyles({
+    root: {
+        color: amber[500],
+        raised: true,
+        // minWidth: '1200px',
+        width:'1000px',
+        // maxWidth: '1500px',
+        letterSpacing: '0.2rem',
+        textDecoration: 'none',
+        padding: '1px',
+        margin: '0.5rem',
+        backgroundColor: deepPurple[500],
+        '&:hover': {backgroundColor: deepPurple[200], color: cyan[400] },
+        //'&:active': {transform: "scale3d(1.05, 1.05, 1)"}
+
+    }
+})(Card);
+
+const BottomButton = withStyles({
+    root: {
+        backgroundColor:amber[500],
+        color: cyan[500],
+        margin: '1px',
+        //padding: '1px',
+        '&:hover': {backgroundColor: amber[100], },
+        '&:active': {transform: "scale3d(1.05, 1.05, 1)"}
+    }
+})(Button);
+
+const Typographyy = withStyles({
+    root: {
+        padding: '10px',
+        width: 'auto',
+        textAlign: 'left',
+        justifyContent: 'left'
+        //margin: '1px'
+    }
+})(Typography);
 
 
 function ListItemQuestion(props) {
@@ -45,46 +124,64 @@ function ListItemQuestion(props) {
 
     return (
         <>
-            <td>{question.id}.  {question.question}</td>
-            <td>
-                <select id = {question.id}>
-                    <option key={0} value="0"></option>
-                    {usersList.map(user => (
-                        <option key={user.id} value={user.id}>
-                            Name: {user.username} User ID: {user.id}
-                        </option>
-                    ))}
-                </select>  
-            </td>
-            
-            <td>{question.username}</td>
-            <td><button onClick={asker}>checkmark</button></td>
-            <td>{(question.approved? 'Yes':'No')}</td>
-            <td>{(question.flagged? 'Yes':'No')}</td>
-            <td>
+        {/* <Cardfront>
+            <Typographyy padding='px' >
+                REF CARD: {question.id}, Flagged?:{(question.flagged? 'Yes':'No')}, Approved?:{(question.approved? 'Yes':'No')}
+            </Typographyy>
+        </Cardfront> */}
+
+        <QuestionCard style={question.approved? {backgroundColor: lightGreen[300]} : {backgroundColor: grey[300]}} >
+            <CardContent>
+                {/* <Typographyy padding='px' >{question.id} USED FOR REFERENCE ONLY</Typographyy> */}
+                <Typographyy>{question.question}, asked by: {question.username}</Typographyy>
+                
+                
+
+                {/* <Typographyy>{question.username}</Typographyy> */}
+                
+            </CardContent>
+            <CardContent>
+                <FormControli>
+
+                    <select id = {question.id}>
+                        <option key={0} value="0"></option>
+                        {usersList.map(user => (
+                            <option key={user.id} value={user.id}>
+                                Name: {user.username} User ID: {user.id}
+                            </option>
+                        ))}
+                    </select>  
+
+                </FormControli>
+                <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+                    <Select>
+                        <MenuItem disabled value="">
+                            <em>Placeholder</em>
+                        </MenuItem>
+                            {usersList.map((user) => (
+                                <MenuItem
+                                    key={user.id}
+                                    value={user.id}
+                                    //style={getStyles(name, personName, theme)}
+                                >
+                                    {user.username}
+                                </MenuItem>
+                            ))}
+                    </Select>
+
+                </FormControl>
+                <BottomButton onClick={asker} >asker</BottomButton>
+                <BottomButton onClick={deleteQuestion}>Delete</BottomButton>
+                <BottomButton onClick={flagQuestion}>Mark As Flagged</BottomButton>
                 {(question.approved?
-                <button onClick={approveQuestion}>Disapprove</button> : 
-                <button onClick={approveQuestion}>Approve</button>)}
-            </td>
-            <td><button onClick={flagQuestion}>Mark As Flagged</button></td>
-            <td><button onClick={deleteQuestion}>Delete</button></td>
+                    <BottomButton onClick={approveQuestion}>Disapprove</BottomButton> : <BottomButton  onClick={approveQuestion}>Approve</BottomButton>
+                )}
+                {/* <Typographyy>Approved?:{(question.approved? 'Yes':'No')}</Typographyy> */}
+                {/* <Typographyy>Flagged?:{(question.flagged? 'Yes':'No')}</Typographyy> */}
+            </CardContent>
+        </QuestionCard>
         </>
     )
-
-//     return ({
-        
-//             userName:question.username,
-//             question: question.question,
-//             approval: {(question.approved? 'Yes':'No')},
-//             flag: {(question.flagged? 'Yes':'No')},
-//             approveButton: {(question.approved?
-//                 <button onClick={approveQuestion}>Disapprove</button> : 
-//                 <button onClick={approveQuestion}>Approve</button>)},
-            
-//             flagButton: <button onClick={flagQuestion}>Mark As Flagged, Archive Me</button>
-//             deleteButton: <button onClick={deleteQuestion}>Delete</button>
-
-//             })
 }
 
 export default ListItemQuestion;
--- src/components/ListOfQuestions/ListOfQuestions.jsx
+++ src/components/ListOfQuestions/ListOfQuestions.jsx
@@ -1,44 +1,31 @@
-import { ListItem } from '@material-ui/core';
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import ListItemQuestion from '../ListItemQuestion/ListItemQuestion';
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-// function createData(
-//     name: string,
-//     calories: number,
-//     fat: number,
-//     carbs: number,
-//     protein: number,
-// ) {
-//     return { name, calories, fat, carbs, protein };
-// }
+import './ListOfQuestions.css';
+import { Button, Typography, Container } from '@material-ui/core';
+import { amber,deepPurple,cyan } from '@mui/material/colors';
+import { ThemeProvider, withStyles} from '@material-ui/core';
+//import Container from '@material-ui/core';
 
 
-// const rows = [
-//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//     createData('Eclair', 262, 16.0, 24, 6.0),
-//     createData('Cupcake', 305, 3.7, 67, 4.3),
-//     createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
 
-
+const BottomButton = withStyles({
+    root: {
+        backgroundColor:amber[500],
+        color: cyan[500],
+        margin: '1px',
+        //padding: '1px',
+        '&:hover': {backgroundColor: amber[100], },
+        '&:active': {transform: "scale3d(1.05, 1.05, 1)"}
+    }
+})(Button);
 
 function ListOfQuestions(){
     const history = useHistory();
     const dispatch = useDispatch();
     const questions = useSelector(store => store.questionList);
-    //const usersList = useSelector(store => store.usersListReducer);
 
     useEffect(() => {
         getQuestions();
@@ -64,156 +51,25 @@ function ListOfQuestions(){
         history.push('/questions');
     }
 
-//: GridColDef[] am i going to need this?
-    const columns = [
-        {
-        field: 'userName',
-        headerName: 'User',
-        width: 150,
-        headerAlign: 'left'
-        },
-        {
-        field: 'question',
-        headerName: 'Question Asked',
-        width: 250,
-        editable: false,
-        headerAlign: 'left'
-        },
-        {
-        field: 'approval',
-        headerName: 'Approval Status',
-        width: 120,
-        editable: true,
-        headerAlign: 'center'
-        },
-        {
-        field: 'flag',
-        headerName: 'Flag Question',
-        //type: 'number',
-        width: 120,
-        editable: true,
-        headerAlign: 'center'
-        },
-        {
-        field: 'approveButton',
-        headerName: 'Approve Button',
-        //type: 'number',
-        width: 120,
-        editable: true,
-        headerAlign: 'center'
-        },
-        {
-        field: 'flagButton',
-        headerName: 'Flag Button',
-        //type: 'number',
-        width: 120,
-        editable: true,
-        headerAlign: 'center'
-        },
-        {
-        field: 'deleteButton',
-        headerName: 'Delete Button',
-            //type: 'number',
-        width: 120,
-        editable: true,
-        headerAlign: 'center'
-        },
 
-    ];
-
-    //const rowData = questions.map(question => (<ListItemQuestion key={question.id} question ={question}/>))
-
-    
 
     return (
         <div>
-            <Box sx={{padding: '40px', width: '90%', justifyContent: 'center', margin: 'auto', display: 'flex' }}>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Question</th>
-                            <th>Who Asked?</th>
-                            <th>Who Submitted</th>
-                            <th>Done</th>
-                            <th>Approved?</th>
-                            <th>Flagged?</th>
-                            <th>Approve</th>
-                            <th>Mark As Flagged</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+            <Box sx={{padding: '10px', width: '90%', justifyContent: 'center', margin: 'auto%', display: 'flex' }}>
+                    <ul className='mainQuestionList'>
                         {questions.map(question => (
-                            <tr key={question.id}>
+                            <li key={question.id}>
                                 <ListItemQuestion key={question.id} question ={question}/>
-                            </tr>
+                            </li>
                         ))}
-                    </tbody>    
-                </table>
+                    </ul>
             </Box>
-
-            {/* <Box sx={{ height: 400, width: '90%', justifyContent: 'center', margin: 'auto', display: 'flex' }}>
-                <DataGrid
-                    rows={questions}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    //checkboxSelection
-                    disableSelectionOnClick
-                    experimentalFeatures={{ newEditingApi: true }}
-                />
-            </Box> */}
-
-            {/* <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                            <TableCell component="th" scope="row">
-                            {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <Box sx={{padding: '40px', width: '90%', justifyContent: 'center', margin: 'auto', display: 'flex' }}>
-                <button onClick={getQuestions}>Refresh Questions List</button>
-                <button onClick={returnToAskQuestions}>Add Questions</button>
-                <button onClick={archiveQuestions}>End Session</button>
+            <Box sx={{padding: '10px', width: '90%', justifyContent: 'center', margin: 'auto', marginBottom:'100px', display: 'flex'}}>
+                <BottomButton onClick={getQuestions}>Refresh Questions List</BottomButton>
+                <BottomButton onClick={archiveQuestions}>End Session / Archive</BottomButton>
+                <BottomButton onClick={returnToAskQuestions}>Add Questions</BottomButton>
             </Box>
-
-
-            {/* <p>{JSON.stringify(usersList)}</p> */}
+            
         </div>
     )
 }
