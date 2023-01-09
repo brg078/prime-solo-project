055bf6da02bcbe1a109c3ee0650ee742a109b0bf
Brett R. Gebbie <brg078@gmail.com>
2023-01-09 10:09:49 -0600

    working on dropdown menus

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-// This is one of our simplest components
-// It doesn't have local state
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is
 
 function InfoPage() {
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <div className="container">
-      <p>Info Page</p>
-    </div>
+    <>
+      <div className="container">
+        <p>Info Page</p>
+      </div>
+    
+      
+        <Box sx={{ minWidth: 120 , maxWidth: 200}}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              placeholder='Select A Student'
+              onChange={handleChange}
+            >
+              <MenuItem value='10'>Ten</MenuItem>
+              <MenuItem value='20'>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+              {/* <MenuItem>
+              <option value={40}>40</option>
+              </MenuItem>
+              <option><MenuItem>dd</MenuItem></option> */}
+            </Select>
+          </FormControl>
+        </Box>
+      
+    </>
   );
 }
+
 
 export default InfoPage;
